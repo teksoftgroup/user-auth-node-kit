599fe5cfdeb0c2b250a413f0e21e887834bb0372
Pascal Anglade <pascal.nglade@gmail.com>
2023-12-25 21:09:22 -0500

    Change models in database migration to entities. Add server configuration. Add useful constants

---
+++ config/server.config.js
@@ -0,0 +1,7 @@
+export const NODE_ENV = process.env.NODE_ENV;
+export const DB_SERVER = process.env.DB_SERVER;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASS = process.env.DB_PASS;
+export const DB_NAME = process.env.DB_NAME;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
---
+++ constants.js
@@ -0,0 +1,18 @@
+export const HTTP_STATUS = Object.freeze({
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  SERVER_ERROR: 500,
+});
+
+export const SECOND = 1000;
+export const MINUTE = 60 * SECOND;
+export const HOUR = 60 * MINUTE;
+export const DAY = 24 * HOUR;
+
+export const allowedOrigins = [""];
