e863d521da6f36a7ee098f8c0dbf4af45b14bfc2
Pascal Anglade <pascal.nglade@gmail.com>
2023-12-25 21:11:28 -0500

    Add user model for direct interaction with database. setup database configuration using mysql2

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import { database } from "../services/mysql";
+
+export const User = {
+  addOne: async function (data) {},
+  clearRefreshToken: async function (id) {},
+  findAll: async function () {},
+  findByField: async function (fieldName, fieldValue) {},
+  findByRefreshToken: async function (refreshToken) {},
+  findByUsername: async function (username) {},
+  findOne: async function (id) {},
+  updateByUsername: async function (username, data) {},
+  updateOne: async function (id, data) {},
+};
---
+++ services/mysql.js
@@ -0,0 +1,33 @@
+import mysql from "mysql2";
+
+import {
+  DB_SERVER,
+  DB_USER,
+  DB_PASS,
+  DB_NAME,
+  NODE_ENV,
+} from "../config/server.config";
+
+const db = mysql
+  .createConnection({
+    host: DB_SERVER,
+    user: DB_USER,
+    password: NODE_ENV == "development" ? undefined : DB_PASS,
+    database: DB_NAME,
+  })
+  .promise();
+
+db.on("error", (error) => {
+  console.error("POOL CONNECTION ERRORED OUT because: ", error.code);
+});
+
+const pool = mysql
+  .createPool({
+    host: DB_SERVER,
+    user: DB_USER,
+    password: NODE_ENV == "development" ? undefined : DB_PASS,
+    database: DB_NAME,
+  })
+  .promise();
+
+export const database = { db, pool };
